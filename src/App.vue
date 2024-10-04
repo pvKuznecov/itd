43f1de5a812ee0592fef6173742ec4e5d01ee66a
kup <p.v.kuznecov@yandex.ru>
2024-10-04 11:17:20 +0700

    Add core.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template src="./main.html"></template>
+<style src="./main.css"></style>
+<script>
+    import HeaderComponent from "@/components/HeaderComponent/HeaderComponent.vue";
+    import FooterComponent from "@/components/FooterComponent/FooterComponent.vue";
+
+    export default {
+        name: "App",
+        components: {
+            HeaderComponent,
+            FooterComponent
+        }
+    }
+</script>
