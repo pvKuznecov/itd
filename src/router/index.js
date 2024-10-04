f81c76589b1f38da006e294f018313566d043885
kup <p.v.kuznecov@yandex.ru>
2024-10-04 15:45:21 +0700

    Add core.

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,10 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/AboutView/AboutView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import(/* webpackChunkName: "about" */ '../views/NotFoundView/NotFoundView.vue')
     }
 ]
 
---
+++ src/views/NotFoundView/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template src="./NotFoundView.html"></template>
+<style src="./NotFoundView.css"></style>
+<script>
+    export default {
+        name: "NotFoundView"
+    }
+</script>
